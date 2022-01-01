becfe1aedced4a5dba4199db9ae1b6bd97243d28
Shadman Ansari <ansarishadman@gmail.com>
2022-01-01 15:17:05 +0530

    Longest prefix suffix

---
+++ String/LongestPrefixSuffix.java
@@ -0,0 +1,29 @@
+public class LongestPrefixSuffix {
+    public static void main(String[] args) {
+        String s = "abab";
+        int ans = lps(s);
+        System.out.println("Count = " + ans);
+    }
+
+    static int lps(String s) {
+        // code here
+        int i = 0, j = 1, n = s.length();
+        int[] lps = new int[n];
+        lps[0] = 0;
+        while (j < n) {
+            if (s.charAt(i) == s.charAt(j)) {
+                lps[j] = i + 1;
+                j++;
+                i++;
+            } else {
+                if (i == 0) {
+                    lps[j] = 0;
+                    j++;
+                } else {
+                    i = lps[i - 1];
+                }
+            }
+        }
+        return lps[n - 1];
+    }
+}
